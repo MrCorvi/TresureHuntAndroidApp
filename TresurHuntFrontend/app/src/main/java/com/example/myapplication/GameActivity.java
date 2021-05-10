88e4c9491fcda77eca2d868d1540bf2e46f31d4d
MrCorvi <39025466+MrCorvi@users.noreply.github.com>
2021-05-10 18:28:02 +0200

    fixato problema che impediva di aprire app

--- TresurHuntFrontend/app/src/main/java/com/example/myapplication/GameActivity.java
+++ TresurHuntFrontend/app/src/main/java/com/example/myapplication/GameActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //set top-bar button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void listStepsClick(View view){
+        //Allow to switch from the current Activity to the next
+        Intent intent = new Intent(GameActivity.this, ListViewActivity.class);
+        startActivity(intent);
     }
 }
