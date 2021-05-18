093762d0d76c94794fb87fea0e440180812e834f
GianmarcoPetrelli <47210869+GianmarcoPetrelli@users.noreply.github.com>
2021-05-18 17:55:58 +0200

    Added base map services

--- TresurHuntFrontend/app/src/main/java/com/example/myapplication/MakerMapActivity.java
+++ TresurHuntFrontend/app/src/main/java/com/example/myapplication/MakerMapActivity.java
@@ -2,16 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.Criteria;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
-public class MakerMapActivity extends AppCompatActivity {
+public class MakerMapActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private int LAUNCH_PLACE = 1;
     private int LAUNCH_CAMERA = 2;
@@ -21,12 +43,43 @@ public class MakerMapActivity extends AppCompatActivity {
 
     private Game game;
 
+    private GoogleMap mMap;
+    final static int PERMISSION_ALL = 1;
+    final static String[] PERMISSIONS = {android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+    //private Geocoder geocoder;
+    private LatLng CurrentLocation;
+    MarkerOptions mo;
+    Marker pos_marker;
+    //private HashMap<LatLng,Marker> GameStepPosition;
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maker_map);
         Intent i = getIntent();
         game = new Game(i.getStringExtra("gameName"));
+        //mapFragment allows to manage map fragment retrieved
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //geocoder = new Geocoder(this, Locale.getDefault());
+        //GameStepPosition = new HashMap<>();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mo = new MarkerOptions().position(new LatLng(0, 0)).title("My Current Location");
+        if (Build.VERSION.SDK_INT >= 23 && !isPermissionGranted()) {
+            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+        } else {
+            //Criteria set parameters to access location service
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setPowerRequirement(Criteria.POWER_HIGH);
+            String provider = locationManager.getBestProvider(criteria, true);
+            locationManager.requestLocationUpdates(provider, 1000, 10, this);
+        }
+        if (!isLocationEnabled())
+            showAlert(1);
     }
 
     public void placeClick(View view){
@@ -104,4 +157,89 @@ public class MakerMapActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        pos_marker =  mMap.addMarker(mo);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        LatLng CurrentCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+        //keep last position registered
+        CurrentLocation = CurrentCoordinates;
+        //update position marker and view
+        pos_marker.setPosition(CurrentCoordinates);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CurrentCoordinates));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean isPermissionGranted() {
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED || checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v("mylog", "Permission is granted");
+            return true;
+        } else {
+            Log.v("mylog", "Permission not granted");
+            return false;
+        }
+    }
+    private void showAlert(final int status) {
+        String message, title, btnText;
+        if (status == 1) {
+            message = "Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                    "use this app";
+            title = "Enable Location";
+            btnText = "Location Settings";
+        } else {
+            message = "Please allow this app to access location!";
+            title = "Permission access";
+            btnText = "Grant";
+        }
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setCancelable(false);
+        dialog.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(btnText, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        if (status == 1) {
+                            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(myIntent);
+                        } else
+                            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        finish();
+                    }
+                });
+        dialog.show();
+
+    }
+
 }
