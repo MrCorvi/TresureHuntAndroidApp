9eff0c5b04e6c8aa688330fe01ede0705e573e9f
Marsha <m.gomezgomez@studenti.unipi.it>
2021-05-30 16:12:15 +0200

    * Changed name. Frontend

--- TreasureHuntFrontend/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TreasureHuntFrontend/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.DialogInterface;
